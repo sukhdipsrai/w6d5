1ac425d43ca56e58b211c0b4f0c6c2f8db54ba6e
Sukhdip Rai <sukhdipsrai@gmail.com>
2020-12-04 17:26:53 -0500

    upto edit form

--- cats_99/app/controllers/cats_controller.rb
+++ cats_99/app/controllers/cats_controller.rb
@@ -8,7 +8,29 @@ class CatsController < ApplicationController
     def show
         @cats = Cat.find_by(id: params[:id])
 
-        render 'show'
+        if @cats
+            render 'show'
+        else
+            redirect_to cats_url
+        end
+    end
+
+    def new
+        render :new
+    end
+
+    def create
+
+    end
+
+    def edit
+        @cats = Cat.find_by(id: params[:id])
+
+        if @cats
+            render :edit
+        else
+            redirect_to cat_url
+        end
     end
 
 end
